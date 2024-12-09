fe4392d36fc4f7930c1160f5f76bc37b513eeb0d
Patrick Candlin <patrick.candlin@gmail.com>
2024-12-08 22:13:43 -0700

    next test pases

--- AoCDay3/ScannerTests/ScannerTests.cs
+++ AoCDay3/ScannerTests/ScannerTests.cs
@@ -14,13 +14,14 @@ public class InstructionsScannerTests
     }
 
     [Theory]
-    [InlineData("mul(2,4)")]
-    public void ValidCommand_ShouldReturnMultipleOfNumbers(string instructions)
+    [InlineData("mul(2,4)", 8)]
+    [InlineData("mul(2,4)mul(2,4)", 16)]
+    public void ValidCommand_ShouldReturnMultipleOfNumbers(string instructions, double expected)
     {
         var instructionScanner = new InstructionScanner(instructions);
         var result = instructionScanner.ParseFromInstructions(instructionScanner.Instructions);
         
-        Assert.Equal(8, result);
+        Assert.Equal(expected, result);
 
     }
 }
