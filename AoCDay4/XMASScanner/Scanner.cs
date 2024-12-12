1b8e51d4875f52fc220181c919f03005b3a5cea6
Patrick Candlin <patrick.candlin@gmail.com>
2024-12-12 09:22:09 -0700

    passed first test

--- AoCDay4/XMASScanner/Scanner.cs
+++ AoCDay4/XMASScanner/Scanner.cs
@@ -2,6 +2,20 @@
 
 public static class Scanner
 {
-    public static int FindAll(string path, string pattern) => 
-    File.ReadLines(path)?.Select(line => line).Count() ?? 0;
+    public static int FindAll(string path, string pattern)
+    {
+        var count = 0;
+
+        try
+        {
+            var lines = File.ReadAllLines(path);
+        }
+        catch (Exception ex)
+        {
+            return 0;
+        }
+        
+        return count;
+    }
+
 }
