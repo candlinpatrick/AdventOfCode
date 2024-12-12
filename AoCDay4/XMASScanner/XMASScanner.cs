9b6c6e9b471621cc0c5c3922581706e3ed2a9e5f
Patrick Candlin <patrick.candlin@gmail.com>
2024-12-12 06:42:43 -0700

    day 4 blank template

---
+++ AoCDay4/XMASScanner/XMASScanner.cs
@@ -0,0 +1,6 @@
+﻿namespace XMASScanner;
+
+public class Class1
+{
+
+}
---
+++ AoCDay4/XMASScannerTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace XMASScannerTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
