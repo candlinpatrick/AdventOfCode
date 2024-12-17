78898633bf2b05f1e64824cd737630d3fbbb4900
Patrick Candlin <patrick.candlin@gmail.com>
2024-12-17 05:25:10 -0700

    missed these changes for the other day

--- AoCDay4/Runner/Program.cs
+++ AoCDay4/Runner/Program.cs
@@ -8,3 +8,7 @@ var lines = File.ReadAllLines("/Users/patrickcandlin/AoC2024/AoCDay4/Input.txt")
 var count = Scanner.FindAll(lines, "XMAS");
 
 Console.WriteLine($"XMAS found: {count} times!");
+
+var count2 = XScanner.FindAll(lines);
+
+Console.WriteLine($"X-MAS found: {count2} times!");
