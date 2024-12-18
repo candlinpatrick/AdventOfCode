6da62834701a23241de8cd18b6b73f058546a5e2
Patrick Candlin <patrick.candlin@gmail.com>
2024-12-17 21:19:54 -0700

    qick save of work

--- AoCDay5/PageOrderer/Orderer.cs
+++ AoCDay5/PageOrderer/Orderer.cs
@@ -1,3 +1,64 @@
 ﻿namespace PageOrderer;
 
-public class Orderer { }
+public static class Orderer
+{
+    public static int GetSumOfMidpoints(string input)
+    {
+        var sum = 0;
+
+        var orderRules = GetOrderingRules(input);
+        var orderedPages = new List<string>();
+        var numbersLists = GetnumberList(input);
+
+        for (int i = 0; i < numbersLists.Count; i++)
+        {
+            List<string>? nums = numbersLists[i];
+
+            for (int j = 0; j < nums.Count - 1; j++)
+            {
+                var num1 = nums[i];
+                var num2 = nums[i + 1];
+
+                var luckyShot = orderRules.Where(x => x.Contains(num1) && x.Contains(num2));
+
+                if (luckyShot.Any() && false) { }
+            }
+        }
+
+        return sum;
+    }
+
+    private static List<List<string>> GetnumberList(string input)
+    {
+        var lines = input.Split('\n');
+
+        var numbers = new List<List<string>>();
+
+        foreach (var line in lines)
+        {
+            if (line.Contains(','))
+            {
+                numbers.Add(line.Split(',').ToList());
+            }
+        }
+
+        return numbers;
+    }
+
+    private static List<string> GetOrderingRules(string input)
+    {
+        var lines = input.Split("\n");
+
+        var rules = new List<string>();
+
+        foreach (var line in lines)
+        {
+            if (line.Contains('|'))
+            {
+                rules.Add(line);
+            }
+        }
+
+        return rules;
+    }
+}
--- AoCDay5/PageOrdererTests/OrdererTests.cs
+++ AoCDay5/PageOrdererTests/OrdererTests.cs
@@ -1,7 +1,16 @@
 namespace PageOrdererTests;
 
+using PageOrderer;
+
 public class PageOrdererTests
 {
     [Fact]
-    public void Test1() { }
+    public void WhenGivenSimpleInputShouldReturn12()
+    {
+        var input = "13|12/n" + "13|22/n" + "12|22/n" + "\n" + "13,12,22";
+
+        var result = Orderer.GetSumOfMidpoints(input);
+
+        Assert.Equal(12, result);
+    }
 }
